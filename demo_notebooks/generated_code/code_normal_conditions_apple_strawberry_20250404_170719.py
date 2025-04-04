5c16d4a414572944fd1940d5ea2e61116414bff7
Takahiro Tsurumaki <tsurumaki.takahiro.r1@dc.tohoku.ac.jp>
2025-04-04 17:36:27 +0900

    add demo notebook

---
+++ demo_notebooks/generated_code/code_normal_conditions_apple_strawberry_20250404_170719.py
@@ -0,0 +1,27 @@
+code="""
+```python
+def execute_command1(image_path, image):
+    image_patch = ImagePatch(image)
+    
+    # Find apples and strawberries in the image.
+    patch_dict = image_patch.find("apple. strawberry")
+    apple_patches = patch_dict["apple"]
+    strawberry_patches = patch_dict["strawberry"]
+    
+    # Since you've detected two or more objects, make sure to use the 'delete_overlaps' function.
+    apple_patches, strawberry_patches = delete_overlaps(apple_patches, strawberry_patches) 
+    
+    # Count the number of apples and strawberries in the image
+    num_apples = len(apple_patches)
+    num_strawberries = len(strawberry_patches)
+    print(f"Number of apples is {num_apples}")
+    print(f"Number of strawberries is {num_strawberries}")
+    
+    # Verify if the count matches the Normal_condition specification.
+    anomaly_score = 0
+    if num_apples != 2 or num_strawberries != 6:
+        anomaly_score = 1
+    
+    return formatting_answer(anomaly_score)
+```
+"""
---
+++ demo_notebooks/normal_conditions_apple_strawberry.py
@@ -0,0 +1,6 @@
+normal_conditions = """
+Normal_condition1:There are two [apple]s and six [strawberry]s in the image.
+Function1:
+
+Create 1 Python function.
+"""
